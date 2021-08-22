d5e8764796f88513585b9b2a52491d0ba71159a1
hakrimryu <hakrimryu@gmail.com>
2021-08-22 19:52:19 +0900

    Prefabs内のMap Rrefabを全て読み込んで、全てのMapデータを出力するように変更

--- Assets/Ryu/Scripts/Editor/MapEditor.cs
+++ Assets/Ryu/Scripts/Editor/MapEditor.cs
@@ -13,39 +13,36 @@ public class MapEditor
     [MenuItem("Tools/GenerateMap %#g")]
     private static void GenerateMap()
     {
-        // Map取得
-        GameObject go = GameObject.Find("Map");
-        if (go == null)
-            return;
-        
-        // Collision Tilemap 取得
-        Tilemap tm = Util.FindChild<Tilemap>(go, "Tilemap_Collision", true);
-        if (tm == null)
-            return;
-        
-        // マップデータ出力
-        using (var writer = File.CreateText("Assets/Ryu/Resources/Map/output.txt"))
-        {
-            writer.WriteLine(tm.cellBounds.xMin);
-            writer.WriteLine(tm.cellBounds.xMax);
-            writer.WriteLine(tm.cellBounds.yMin);
-            writer.WriteLine(tm.cellBounds.yMax);
+        GameObject[] gameObjects = Resources.LoadAll<GameObject>("Prefabs/Map");
 
-            // 左上から始めて右下に出力
-            for (int y = tm.cellBounds.yMax; y >= tm.cellBounds.yMin; y--)
+        foreach (GameObject go in gameObjects)
+        {
+            // Collision Tilemap 取得
+            Tilemap tm = Util.FindChild<Tilemap>(go, "Tilemap_Collision", true);
+        
+            // マップデータ出力
+            using (var writer = File.CreateText($"Assets/Ryu/Resources/Map/{go.name}.txt"))
             {
-                for (int x = tm.cellBounds.xMin; x <= tm.cellBounds.xMax; x++)
+                writer.WriteLine(tm.cellBounds.xMin);
+                writer.WriteLine(tm.cellBounds.xMax);
+                writer.WriteLine(tm.cellBounds.yMin);
+                writer.WriteLine(tm.cellBounds.yMax);
+
+                // 左上から始めて右下に出力
+                for (int y = tm.cellBounds.yMax; y >= tm.cellBounds.yMin; y--)
                 {
-                    TileBase tile = tm.GetTile(new Vector3Int(x, y, 0));
-                    if (tile != null)
-                        writer.Write("1");
-                    else
-                        writer.Write("0");
+                    for (int x = tm.cellBounds.xMin; x <= tm.cellBounds.xMax; x++)
+                    {
+                        TileBase tile = tm.GetTile(new Vector3Int(x, y, 0));
+                        if (tile != null)
+                            writer.Write("1");
+                        else
+                            writer.Write("0");
+                    }
+                    writer.WriteLine();
                 }
-                writer.WriteLine();
             }
         }
-
     }
 
 
